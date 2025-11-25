4cd36be8deab84cc94a214209b17bbf253e08188
Mahiro-tan <21114@hakodate.kosen-ac.jp>
2025-11-25 09:56:24 +0900

    takaraを初期配置して表示、サイズ変更

---
+++ game6/takara.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class takara here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class takara extends Actor
+{
+    /**
+     * Act - do whatever the takara wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        GreenfootImage img = getImage(); // 現在の画像を取得
+        img.scale(50, 50);               // 幅50, 高さ50に変更
+        setImage(img); 
+        // Add your action code here.
+    }    
+}
