bed99865b48d54b34fb04b8e2e9d0c88fafdf38d
nakanomeisei <21123@hakodate.kosen-ac.jp>
2026-01-13 10:06:20 +0900

    メッセージ表示

--- game6/MyWorld.java
+++ game6/MyWorld.java
@@ -1,14 +1,13 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+ 
 /**
- * Write a description of class MyWorld here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+* Write a description of class MyWorld here.
+* 
+* @author (your name) 
+* @version (a version number or a date)
+*/
 public class MyWorld extends World
 {
-    
     private boolean started = false;
     /**
      * Constructor for objects of class MyWorld.
@@ -18,13 +17,26 @@ public class MyWorld extends World
     {    
         super(600, 400, 1); 
         getBackground().scale( 600, 400 );
+        showText("PRESS 'S' TO START", 200, 140 );
+        showText("'SPACE' でROBOを停止", 200, 170 );
+        showText("お宝に近いと高得点!", 200, 190 );
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+
+    }
+    public void act()
+    {
+        if (!started && Greenfoot.isKeyDown("S")) {
+            started = true;
+            startGame();
+            showText("", 200, 140 );
+            showText("", 200, 170 );
+            showText("", 200, 190 );
+        }
+    }
+    private void startGame()
+    {
         addObject( new takara(), 20, 200);
         addObject( new ROBO(), 580, 200 );
-        
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        
-        
-        
+        // 他のActorもここで追加
     }
-   
 }
--- game6/ROBO.java
+++ game6/ROBO.java
@@ -1,11 +1,11 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+ 
 /**
- * Write a description of class ROBO here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+* Write a description of class ROBO here.
+* 
+* @author (your name) 
+* @version (a version number or a date)
+*/
 public class ROBO extends Actor
 {
     /**
@@ -18,45 +18,65 @@ public class ROBO extends Actor
         int i = 0;
         int N = 10;
         int score = 0;
+        int now = 0;
     public void act() 
     {
         // Add your action code here.
-       
         getImage().scale( 100, 100 );
         setRotation(0);
         getWorld().showText("SCORE:"+score, 390, 50 );
-        getWorld().showText(11-N+"お掃除目　SPACEで停止", 130, 20 );
-
+        getWorld().showText(11-N+"お掃除目", 100, 20 );
+ 
         if (Greenfoot.isKeyDown("space")) {
             A=0;
             B=0;
             int x= getX();
-            score+=116-0.2*x;
+            now = 0;
+            now += 116-0.2*x;
+            score += now;
             getWorld().showText("SCORE:"+score, 390, 50 );
             N--;
             if( N <= 0){
                 getWorld().showText( "CLEAR", 395, 170 );
                 Greenfoot.stop();
             }
+            
+            if( now <= 20){
+                getWorld().showText( "BAD", 60, 140 );
+            }
+            else if( now <= 40){
+                getWorld().showText( "NICE", 60, 140 );
+            }
+            else if( now <= 60){
+                getWorld().showText( "GOOD", 60, 140 );
+            }
+            else if( now <= 80){
+                getWorld().showText( "FUNKY", 60, 140 );
+            }
+            else if(now < 100){
+                getWorld().showText( "PERFECT", 60, 140 );
+            }
+            else{
+                getWorld().showText( "GOD!!!!!!!!!!!", 60, 140 );
+            }
+            
+            
             Greenfoot.delay(100);
+            getWorld().showText( "", 60, 140 );
             setLocation( 580,200 );
             A = -5-(10-N)*1.5;
             B = -1;
-            
         }
         i++;
         if(i%5==1){
             C = (int)A + (int)(Math.random()*((B-A)+1));
         }   
-   
         move(C);
-       
-        
+
         int x= getX();
         if( x < 80){
             getWorld().showText( "GAME OVER", 395, 170 );
             Greenfoot.stop();
         }
-        
     }    
 }
